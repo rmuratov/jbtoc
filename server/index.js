c57f9465c8c392e78c48783f73946166b8317546
Ramil Muratov <ramil@muratov.space>
2023-08-18 14:01:41 +0400

    local server setup

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fs = require('fs');
+
+const host = 'localhost';
+const port = 3001;
+
+const HelpTOC = fs.readFileSync(`${__dirname}/HelpTOC.json`).toString();
+
+const requestListener = function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  res.writeHead(200);
+
+  res.end(HelpTOC);
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { useEffect } from 'react';
+
 import s from './App.module.scss';
 import { TableOfContents } from './TableOfContents';
 
 function App() {
+  useEffect(() => {
+    fetch('http://localhost:3001')
+      .then(res => res.json())
+      .then(r => console.log(r));
+  }, []);
+
   return (
     <div className={s.app}>
       <aside>
